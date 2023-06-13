49e27f859d066bdba62158bd60521f1380dc4d88
Nolwenn <duflos.nol@laposte.net>
2023-06-13 14:32:22 +0200

    La fonction If et le statement else if

---
+++ sololearn2.js
@@ -0,0 +1,42 @@
+let num = 12;
+
+if(num > 5) {
+    console.log('Bigger than 5');
+    if(num <= 47){
+        console.log('Betwenn 5 and 47');
+    }
+}
+
+let age= 17;
+if(age <= 19) {
+    console.log("Take your kindle");
+}
+
+let score = 98;
+
+if(score >=100){
+    console.log("Niveau complété :D");
+} else {
+    console.log("essais encore :'(");
+}
+
+// let color = "green";
+
+// if (color == "red") {
+//     console.log("1");
+// } else if (color == "green") {
+//     console.log("2");
+// } else (color == "black") {
+//     console.log("3");
+// }
+
+let color = 'red';
+if (color == 'red'){
+    console.log("1");
+} else if (color == 'green'){
+    console.log('2');
+} else if (color == "black"){
+    console.log('3');
+} else (
+    console.log('unknown')
+)
