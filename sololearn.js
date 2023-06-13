66a5fd63629607a7bfc0c7a7464f2bfe84c7970a
Nolwenn <duflos.nol@laposte.net>
2023-06-13 10:49:57 +0200

    Execution des exercices

--- sololearn.js
+++ sololearn.js
@@ -1,2 +1,31 @@
+//Premières notion de JS via Sololearn
+//Cool, encore des trucs que je connait -_-
 console.log("ça va la vie ?");
-alert("Ceci est une alerte, tu ferais mieux de la quitter");
+//alert("Ceci est une alerte, tu ferais mieux de la quitter");
+console.log((60*60)*(24*30));
+
+// let sum = 0;
+// while(true) {
+//     let value = +prompt("Entrez un nombre", '');
+//     if(!value)break; //(*)
+
+//     sum += value;
+// }
+
+// alert('Sum: ' + sum);
+
+let name; 
+name = "James";
+let age = 42;
+age = 50;
+console.log(age)
+
+// const dimanche = "weekend";
+// console.log(dimanche);
+// dimanche = travail; //Celà créera un message d'erreur.
+
+let x = 54;
+let y = 6;
+console.log(x*y);
+console.log(x/y);
+
